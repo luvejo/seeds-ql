55a0d739999cdfb8a176215b389c39e249c9f340
Luis Velásquez <luvejo.dev@gmail.com>
2022-07-20 05:52:29 -0500

    build: setup TailwindCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import { Hydrate, QueryClientProvider } from 'react-query'
 import type { AppProps } from 'next/app'
 import { queryClient } from '@/src/api'
 
+import '@/assets/main.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
